58af57c922347a9bcd4702b4c8d3606c96e06efd
pouyesh <pouyesh1362@hotmail.com>
2020-08-27 15:38:29 -0700

    Set up app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const forcast = require('./utils/forcast');
 
 const path = require('path');
 const express = require('express');
+const port = process.env.PORT || 3000;
 
 const hbs = require('hbs');
 const { title } = require('process');
@@ -99,6 +100,6 @@ app.get('*', (req, res)=>{
 })
 
 
-app.listen(3000,()=>{
-  console.log(`server is out on port 3000...`);
+app.listen(port,()=>{
+  console.log(`server is out on port ${port}...`);
 })
